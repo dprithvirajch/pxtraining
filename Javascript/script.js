900339cc5df2abebb2a70645d2b3dc6558a4b3fc
chlalithakrishna <lalithakrishna.ch@gmail.com>
2023-03-31 11:04:02 +0530

    newdemosetup

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,4 @@
 
-
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-BOFSTKFNWCGO-2");
-
-
-
 function allowlogin(usermail)
 {
     var a = document.getElementById("usermail").value;
@@ -15,16 +6,6 @@ function allowlogin(usermail)
     { 
         var b= a.substr(0,5);
         alert('valid user');
-        aptrinsic("identify",
-            {
-
-                "id": b,
-                "email": a
-            },
-            {
-                "id": "GS",
-                "name": "Gainsight"
-            }); 
         location.href = "Page1.html";
     }
     else if(a == '')
