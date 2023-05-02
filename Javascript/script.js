92bd11798ada6b28dc565b9f5248f6d182090aa8
chlalithakrishna <120158573+chlalithakrishna@users.noreply.github.com>
2023-05-02 11:17:18 +0530

    RemovePXTag

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,28 +1,10 @@
 
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-  var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-  var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-BOFSTKFNWCGO-2");
-
 function allowlogin(usermail)
 {
     var a = document.getElementById("usermail").value;
     if (a == 'demouser@gmail.com' || a == 'lkchittajallu@gainsight.com' || a == 'dpraj@gainsight.com')
     { 
         var b= a.substr(0,5);
-aptrinsic("identify",
-{
-//User Fields
-  "id": b, // Required for logged in app users
-  "email":a,
-  "firstName": "Lalitha"
-},
-{
-//Account Fields
-  "id":"GS", //Required
-  "name":"Gainsight",
-});
         alert('valid user');
         location.href = "Page1.html";
     }
