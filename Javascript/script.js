1f03511c3f08476a0df7752f5672f75d93918f81
Diwakar Prithviraj <120359969+dprithvirajch@users.noreply.github.com>
2023-05-02 15:13:57 +0530

    Removed the Aptrinsic code

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,5 @@
 
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-OYIBE2USXLTJ-2");
+
 
 function allowlogin(usermail)
 {
@@ -11,21 +7,6 @@ function allowlogin(usermail)
     if (a == 'demouser@gmail.com' || a == 'lkchittajallu@gainsight.com')
     { 
         var b= a.substr(0,5);
-      
-      //passing user and account objects:
-aptrinsic("identify",
-  {
-  //User Fields
-    "id": b, // Required for logged in app users
-    "email": a,
-    "firstName": "John",
-    "lastName": "Smith"
-  },
-  {
-  //Account Fields
-    "id":"AM", //Required
-    "name":"Amazon"
- });
       
         alert('valid user');
         location.href = "Page1.html";
