45523457a0eff298ac7c53da63b0acba377ea52f
Diwakar Prithviraj <120359969+dprithvirajch@users.noreply.github.com>
2023-05-04 17:02:26 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -9,7 +9,7 @@
 function allowlogin(usermail)
 {
     var a = document.getElementById("usermail").value;
-    if (a == 'demouser@gmail.com' || a == 'lkchittajallu@gainsight.com' || a == 'dprithviraj@gainsight.com')
+    if (a == 'demouser@gmail.com' || a == 'lkchittajallu@gainsight.com' || a == 'dpraj@gainsight.com')
     { 
         var b= a.substr(0,5);
       
